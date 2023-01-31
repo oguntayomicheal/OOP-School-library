725a43bec42b847c91abeb6c938345d02b604d42
oguntayomicheal <oguntayomicheal@gmail.com>
2023-01-31 22:13:26 +0100

    Update the classes

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
-./require './person'
+require './person'
 
 class Student < Person
-    def initialize(name = "unknown", age, parent_permission = true, classroom)
-        super(age, name, parent_permission)
-        @classroom = classroom
-    end
+  def initialize(classroom, name = "unknown", age, parent_permission: true)
+    super(name, age)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
